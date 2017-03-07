3c0bb7c97416a1110099908b2be532e4deaa0cf3
Dustin Kahn <dustin@dustinkahn.com>
2017-03-06 20:17:35 -0500

    render json added to create method of api user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,11 +3,13 @@ class Api::V1::UsersController < ApiController
   def index
     @users = User.all
 
-    render json: { users: @users}, s tatus: :ok
+    render json: { users: @users }, status: :ok
   end
 
   def create
     @user = User.create(user_params)
+
+    render json: { user: @user }, status: :ok
   end
 
   private
