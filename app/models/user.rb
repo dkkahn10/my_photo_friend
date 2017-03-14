882ef70a71fed523058c5500e096649b43e3e612
Dustin Kahn <dustin@dustinkahn.com>
2017-03-13 20:31:28 -0400

    paperclip model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,37 +2,40 @@ class User < ApplicationRecord
   attr_accessor :remember_token
   validates :first_name, presence: true
   validates :last_name, presence: true
-  
+
   validates :password, presence: true, length: { minimum: 6 }
   validates :email, presence: true,
     format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i },
     uniqueness: { case_sensitive: false }
-  
+
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
   has_secure_password
-  
+
   # Returns the hash digest of the given string.
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
-  
+
   # Returns a random token.
   def self.new_token
     SecureRandom.urlsafe_base64
   end
-  
+
   # Remembers a user in the database for use in persistent sessions.
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
-  
+
   # Returns true if the given token matches the digest.
   def authenticated?(remember_token)
     return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
-  
+
   # Forgets a user.
   def forget
     update_attribute(:remember_digest, nil)
